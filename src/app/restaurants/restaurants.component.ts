57b410ee45286f61ce359d4a2faf307aa224e586
Rômulo Martins <rm_romulomartins@hotmail.com>
2017-10-11 01:21:28 -0300

    Criando os componentes de Restaurante

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,3 +1,4 @@
+import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './restaurants.component.html'
 })
 export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[] = [
+    {
+      id: "bread-bakery",
+      name: "Bread & Bakery",
+      category: "Bakery",
+      deliveryEstimate: "25m",
+      rating: 4.9,
+      imagePath: "assets/img/restaurants/breadbakery.png"
+    },
+    {
+      id: "burger-house",
+      name: "Burger House",
+      category: "Hamburgers",
+      deliveryEstimate: "100m",
+      rating: 3.5,
+      imagePath: "assets/img/restaurants/burgerhouse.png"      
+    }
+  ]
 
   constructor() { }
 
