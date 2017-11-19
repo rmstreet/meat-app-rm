40d235ed12b5b6b261b6cc8e6576dc77ee4a3481
Rômulo Casa <rm_romulomartins@hotmail.com>
2017-11-19 15:57:41 -0200

    Correção no tratamento de erro para o angular 4.3; Criando Componentes de Detalhes de Restaurante

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,11 +1,11 @@
-import {HttpResponse} from '@angular/common/http'
+import {HttpErrorResponse} from '@angular/common/http'
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/observable/throw'
 
 export class ErrorHandler {
-    static handleError(error: Response | any){
+    static handleError(error: HttpErrorResponse | any){
         let errorMessage: string
-        if(error instanceof HttpResponse){
+        if(error instanceof HttpErrorResponse){
             errorMessage = `Error ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
         }else{
             errorMessage = error.toString()
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router'
 
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { AboutComponent } from './about/about.component';
 
 
@@ -9,5 +10,6 @@ import { AboutComponent } from './about/about.component';
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent},
     {path: 'about', component: AboutComponent}
 ]
